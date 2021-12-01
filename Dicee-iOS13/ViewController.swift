64578c569b6fa90a8f57b8eee50b54457d68a158
Beno Suriano <bjsuriano@gmail.com>
2021-12-01 12:38:32 -0400

    Rolling like a baller.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -16,16 +16,32 @@ class ViewController: UIViewController {
     
     let diceFaces = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
     
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         rollButton.layer.cornerRadius = 60 / 4
         
     }
 
+    @objc func stopRollingAnimation() {
+        diceImageView1.stopAnimating()
+        diceImageView2.stopAnimating()
+    }
+    
     @IBAction func rollButtonPressed(_ sender: Any) {
         
-        print(sender)
+        timer.invalidate()
+        
+        diceImageView1.animationImages = diceFaces.shuffled()
+        diceImageView2.animationImages = diceFaces.shuffled()
+        diceImageView1.animationDuration = 1.0
+        diceImageView2.animationDuration = 0.2
+        
+        diceImageView1.startAnimating()
+        diceImageView2.startAnimating()
+        
+        timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector((stopRollingAnimation)), userInfo: nil, repeats: false)
         
         diceImageView1.image = diceFaces.randomElement()
         diceImageView2.image = diceFaces.randomElement()
